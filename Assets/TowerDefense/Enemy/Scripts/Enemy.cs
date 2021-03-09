4c272a80d92b59f5df88cda3820108e8777b340c
Ciobanu Andrei-Florin <andrei1805florin@gmail.com>
2021-03-09 12:10:45 +0200

    Moved enemy movement logic on Enemy script

--- Assets/TowerDefense/Enemy/Scripts/Enemy.cs
+++ Assets/TowerDefense/Enemy/Scripts/Enemy.cs
@@ -7,21 +7,49 @@
 
 using UnityEngine;
 
+using TowerDefense.Map.Scripts;
+
 namespace TowerDefense.Enemy.Scripts {
 	public class Enemy : MonoBehaviour {
 		
 		[SerializeField] 
 		private float _speed;
 		
+		private WaypointManager _waypointManager => WaypointManager.Instance;
+		private int _waypointIndex = 0;
+		private Vector3 _originalPosition;
+		
 		#region Lifecycle
+
+		private void Awake() {
+			this._originalPosition = this.transform.position;
+		}
+
+		private void Update() {
+			if (this._waypointManager.GetWaypointsArrayLength().Equals(this._waypointIndex)) {
+				this._waypointIndex = 0;
+				this.transform.position = this._originalPosition;
+
+			}
+			this.MoveEnemyTowardsWaypoint(this._waypointManager.GetWaypointAtIndex(this._waypointIndex));
+			this.CheckDistanceBetweenEnemyAndWaypoint();
+		}
 		
 		#endregion
 		
-		#region Public
+		#region Private
 
-		public void MoveEnemyTowardsWaypoint(Transform waypoint) {
+		private void MoveEnemyTowardsWaypoint(Transform waypoint) {
 			Vector3 distance = waypoint.position - this.transform.position;
 			this.transform.Translate(this._speed * Time.deltaTime * distance.normalized, Space.World);
+		}
+		
+		private void CheckDistanceBetweenEnemyAndWaypoint() {
+			float distance = Vector3.Distance(this.transform.position, this._waypointManager.GetWaypointAtIndex(this._waypointIndex).position);
+
+			if (distance <= 0.5f) {
+				this._waypointIndex++;
+			}
 		}
 		
 		#endregion
