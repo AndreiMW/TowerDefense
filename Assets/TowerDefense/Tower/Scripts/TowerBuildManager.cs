082fe4ac50b009ab3a7120d8f021697c61055ed4
Ciobanu Andrei-Florin <andrei1805florin@gmail.com>
2021-03-10 14:59:09 +0200

    Added TowerBuildManager singleton

---
+++ Assets/TowerDefense/Tower/Scripts/TowerBuildManager.cs
@@ -0,0 +1,37 @@
+/**
+ * Created Date: 3/10/2021
+ * Author: Andrei Ciobanu
+ * 
+ * Copyright (c) 2021 ATiStudios. All rights reserved. 
+ */
+
+using System;
+using UnityEngine;
+
+namespace TowerDefense.Tower.Scripts {
+	public class TowerBuildManager : MonoBehaviour {
+		public static TowerBuildManager Instance;
+
+		[SerializeField]
+		private GameObject _towerPrefab;
+		
+		#region Lifecycle
+
+		private void Awake() {
+			if (!Instance) {
+				Instance = this;
+			}
+		}
+
+		#endregion
+		
+		#region Public
+
+		public GameObject BuildTower(Transform targetNodeToInstantiate) {
+			GameObject tower = Instantiate(this._towerPrefab, targetNodeToInstantiate.position, targetNodeToInstantiate.rotation);
+			return tower;
+		}
+		
+		#endregion
+	}
+}
