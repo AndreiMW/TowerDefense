6af43ef7199249072f76a5288a2f99546fe59ccb
Andrei <andreibvmw@gmail.com>
2021-03-13 21:48:24 +0200

    Added game music controller

---
+++ Assets/TowerDefense/GameMusicController.cs
@@ -0,0 +1,26 @@
+/**
+ * Created Date: 3/13/2021
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using UnityEngine;
+
+namespace TowerDefense {
+	public class GameMusicController : MonoBehaviour {
+		private static GameMusicController s_instance;
+		#region Lifecycle
+		
+		private void Awake() {
+			if (!s_instance) {
+				s_instance = this;
+				DontDestroyOnLoad(this.gameObject);
+			} else {
+				Destroy(this.gameObject);
+			}
+		}
+		
+		#endregion
+	}
+}
