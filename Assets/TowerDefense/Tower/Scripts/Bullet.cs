cc6eef01face2dd4855eff2735daa87c27624895
Andrei <andreibvmw@gmail.com>
2021-03-13 14:26:24 +0200

    Fixed bullet going after target when target was dead

--- Assets/TowerDefense/Tower/Scripts/Bullet.cs
+++ Assets/TowerDefense/Tower/Scripts/Bullet.cs
@@ -9,26 +9,33 @@ using System;
 
 using UnityEngine;
 
+using TowerDefense.Enemy.Scripts;
+
 namespace TowerDefense.Tower.Scripts {
 	public class Bullet : MonoBehaviour {
-		private Transform _target;
+		private EnemyComponent _target;
 		[SerializeField] 
 		private float _speed;
-		
 
 		private Vector3 _originalPosition;
 		private int _bulletIndex;
 		public event Action OnBulletReachedEnemy;
 
 		private int _bulletDamage;
+		private bool _shouldResetBulletIfTargetWasLost = true;
 
 		#region Lifecycle
 
 		private void Update() {
-			if (!this._target) {
+			if (!this._target || !this._target.gameObject.activeInHierarchy){
+				if (this._shouldResetBulletIfTargetWasLost) {
+					this.BulletReachedEnemy();
+					this._shouldResetBulletIfTargetWasLost = false;
+				}
 				return;
 			}
-			Vector3 bulletDirection = this._target.position - this.transform.position;
+			
+			Vector3 bulletDirection = this._target.transform.position - this.transform.position;
 			float distanceThisFrame = this._speed * Time.deltaTime;
 			this.transform.Translate(bulletDirection.normalized * distanceThisFrame, Space.World);
 		}
@@ -41,7 +48,8 @@ namespace TowerDefense.Tower.Scripts {
 		
 		#region Public
 		
-		public void SetTargetToFollow(Transform target) {
+		public void SetTargetToFollow(EnemyComponent target) {
+			this._shouldResetBulletIfTargetWasLost = true;
 			this._target = target;
 		}
 
